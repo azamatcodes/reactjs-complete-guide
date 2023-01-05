c5cdd9d995ee3da2ef626ade61703fb362b3cfe0
azamatyudaev <azamatyudaev@gmail.com>
2023-01-06 02:06:54 +0500

    Remove console

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,8 +1,6 @@
 import './ExpenseDate.css'
 
 export const ExpenseDate = (props) => {
-  console.log(props)
-
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
