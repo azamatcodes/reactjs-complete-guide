4ebd5eed46f2b181f56f5787b1b016b190f09cde
azamatyudaev <azamatyudaev@gmail.com>
2023-01-05 15:52:59 +0500

    Adding basic css styling

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 export const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>January 5th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   )
