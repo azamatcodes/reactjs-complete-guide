0ed7d75a359fa89f0d4660c4463fc4e38d5cd2f3
azamatyudaev <azamatyudaev@gmail.com>
2023-01-05 16:13:03 +0500

    Dynamic data

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 export const ExpenseItem = () => {
+  const expenseDate = new Date(2023, 0, 5)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
+
   return (
     <div className="expense-item">
-      <div>January 5th 2023</div>
+      <div>{expenseDate.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   )
